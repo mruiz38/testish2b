2851729886fb889398f8796fcea1f1f94c4f6a90
mruiz38 <marco.ruiz@ucsp.edu.pe>
2021-10-27 16:49:01 -0500

    ultco

--- actividad6/act6.cpp
+++ actividad6/act6.cpp
@@ -6,6 +6,16 @@
 using namespace std;
 
 
+
+void imprimir ( int *p, int dim){
+   int *lim=p; 
+   while(lim< p + dim ){
+      cout<<*lim <<" ";
+      lim++;
+    }
+   cout<<endl;
+}
+
 int sumaMedianaVector( int *mv, int dim1 ){
     int dimension=dim1;
     if(!(dimension%2))   
@@ -40,30 +50,6 @@ while(q < p  + (dim1 * dim2 )  ){
 
 
 
-float rectangulo(float base, float altura) {
-	cout << "El area del rectangulo es: " << base * altura << endl;
-	return base * altura;
-}
-float circulo(float radio, float zero) {
-	float pi = 3.1416;
-	cout << "El area del circulo es: " << pi * (radio * radio) << endl;
-	return pi * (radio * radio);
-}
-float triangulo(float base, float altura) {
-	cout << "El area del triangulo es: " << (base * altura) / 2 << endl;
-	return (base * altura) / 2;
-}
-float cuadrado(float lado, float zero) {
-	cout << "El area del cuadrado es: " << lado * lado << endl;
-	return lado * lado;
-}
-
-typedef float (*PF)(float, float);
-
-float calcularArea(PF pf,float a, float b) {
-  float area = pf(a,b);
-  return area;
-}
 
 typedef int(*PF2)( int *, int);
 int calcularMedianas(PF2 pf2x, int *pt, int dim){
@@ -82,24 +68,7 @@ int main() {
   };
   
   int (*pfiguras)[10] = vectorMedianas;
-  /*	
-  PF PuntFun[4] = {rectangulo,circulo,triangulo,cuadrado};
-  PF* ppff = PuntFun;
-  float figuras[4][2]={{2,0},{3,5},{10,0},{5,3}};//figuras
-  //int num[10]={10,15,5,38,89,77,100,45,48,3};
-  int op[4]= {3,2,1,0};//configuracion de figuras
-  int* pop = op; 
-  float area;
-  float (*pfiguras)[2] = figuras;
-  for(int i =0; i <4; i++)
-  { 
-    calcularArea( (*(ppff+*pop))  ,   (*pfiguras)[0]  ,    (*pfiguras)[1]);
-    //(*(ppff+*pop))((*pfiguras)[0],(*pfiguras)[1]);
-    pop++;
-    pfiguras++;
-  }
-  */
-
+  
   int medianapar[10]={10,20,30,40,50,60,70,80,90,100};
   int medianaimp[3]={1,2,3};
    
@@ -110,32 +79,26 @@ int main() {
 	  {4,3,2,1}
   };
   int (*pmatriz)[4] = matriz;
-
+  int *ptrMatriz = *(matriz);
   int op[2]={0,1};
   int *pop = op;
-  //creando arreglo de punteros
-   int *arregloPuntero[2];
-     *arregloPuntero[0]=  pfiguras;
-
-     //, &pmatriz}; 
-   //cout
-  //int (*p)
-  
+   
   for (int i =0; i<4; i++){
-     
+     //empieza llamando a la funcion para  los arreglos unidimensionales
+     imprimir(*pfiguras,10); 
      cout<<calcularMedianas( (*( ppff2 + *pop  )) , *pfiguras, 10      )<< " "<<endl;
+     
      pfiguras++;
   }
+   cout<<endl;
+
+  //ahora hago la llamada para el calculo de  la mediana de la matriz
+  ///incremento el puntero  pop
+    pop++;
+    cout<<calcularMedianas( (*( ppff2 + *pop  )) , *pmatriz, 4      )<< " "<<endl;
 
 
-//cout<<"sumaMedianaMatriz : "<<sumaMedianaMatriz( matriz[0],4,4  )<<" "<<endl;  
-  
-  
-
-  
-  //cout<<"imprimo el valor de mediana, antes de enviarlo "<<mediana<<endl;; 
-  //cout<<"resultado"<<sumaMedianaVector(medianapar,10)<<endl; 
-  //cout<<"resultado mediana impar"<<sumaMedianaVector(medianaimp,3)<<endl;
+    
   
 
 
